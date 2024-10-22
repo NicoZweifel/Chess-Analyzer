9d2b18e82bb569c95faaee564e3ac6980772fb51
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-10-22 12:37:49 +0200

    save

--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,5 @@ use bevy::prelude::*;
 use crate::Game;
 
 pub(crate) fn startup(mut commands: Commands) {
-    let game = Game::default();
-    commands.spawn(game);
+    commands.spawn(Game::default());
 }
